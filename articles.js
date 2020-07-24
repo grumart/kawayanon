37aaf7092f7dcd00fc0c1deee695922881ec536d
Marta GRudzien <grudzien.mart@gmail.com>
2020-07-24 15:50:40 -0400

    js connected to html

--- articles.js
+++ articles.js
@@ -1,65 +1,85 @@
-import img1 from "../img/bamboo.jpg";
+// import img1 from "../img/bamboo.jpg";
 
 var articles = [{
     id: "1",
     title: "title 1",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    imgsrc: img1,
+    // imgsrc: img1,
     type: "xl"
 }, {
     id: "2",
     title: "title 2",
     body: "Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    imgsrc: img1,
+    // imgsrc: img1,
     type: "l"
 }, {
     id: "3",
     title: "title 3",
     body: "Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    imgsrc: img1,
+    // imgsrc: img1,
     type: "l"
 }, {
     id: "4",
     title: "title 4",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    imgsrc: img1,
+    // imgsrc: img1,
     type: "m"
 }, {
     id: "5",
     title: "title 5",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    imgsrc: img1,
+    // imgsrc: img1,
     type: "m"
 }, {
     id: "6",
     title: "title 6",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    imgsrc: img1,
+    // imgsrc: img1,
     type: "m"
 }, {
     id: "7",
     title: "title 7",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    imgsrc: img1,
+    // imgsrc: img1,
     type: "s"
 }, {
     id: "8",
     title: "title 8",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    imgsrc: img1,
+    // imgsrc: img1,
     type: "s"
 }, {
     id: "9",
     title: "title 9",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    imgsrc: img1,
+    // imgsrc: img1,
     type: "s"
 }, {
     id: "10",
     title: "title 10",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    imgsrc: img1,
+    // imgsrc: img1,
     type: "s"
 }];
 
-export default articles;
+var card0 = document.getElementById("card--0");
+var card1 = document.getElementById("card--1");
+var card2 = document.getElementById("card--2");
+var card3 = document.getElementById("card--3");
+var card4 = document.getElementById("card--4");
+var card5 = document.getElementById("card--5");
+var card6 = document.getElementById("card--6");
+var card7 = document.getElementById("card--7");
+var card8 = document.getElementById("card--8");
+var card9 = document.getElementById("card--9");
+
+var cards = [card0, card1, card2, card3, card4, card5, card6, card7, card8, card9];
+
+function combine(cardId, id) {
+    var cardTitle = cardId.querySelector("h3").innerText = articles[id].title;
+    var cardBody = cardId.querySelector("p").innerText = articles[id].body;
+}
+
+for (var i = 0; i < articles.length; i++) {
+    combine(cards[i], i);
+}
--- src/articles.js
+++ src/articles.js
@@ -1,76 +1,97 @@
-import img1 from "../img/bamboo.jpg";
+// import img1 from "../img/bamboo.jpg";
 
 const articles = [
     {
         id: "1",
         title: "title 1",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        imgsrc: img1,
+        // imgsrc: img1,
         type: "xl"
     },
     {
         id: "2",
         title: "title 2",
         body: "Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        imgsrc: img1,
+        // imgsrc: img1,
         type: "l"
     },
     {
         id: "3",
         title: "title 3",
         body: "Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        imgsrc: img1,
+        // imgsrc: img1,
         type: "l"
     },
     {
         id: "4",
         title: "title 4",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        imgsrc: img1,
+        // imgsrc: img1,
         type: "m"
     },
     {
         id: "5",
         title: "title 5",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        imgsrc: img1,
+        // imgsrc: img1,
         type: "m"
     },
     {
         id: "6",
         title: "title 6",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        imgsrc: img1,
+        // imgsrc: img1,
         type: "m"
     },
     {
         id: "7",
         title: "title 7",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        imgsrc: img1,
+        // imgsrc: img1,
         type: "s"
     },
     {
         id: "8",
         title: "title 8",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        imgsrc: img1,
+        // imgsrc: img1,
         type: "s"
     },
     {
         id: "9",
         title: "title 9",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        imgsrc: img1,
+        // imgsrc: img1,
         type: "s"
     },
     {
         id: "10",
         title: "title 10",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        imgsrc: img1,
+        // imgsrc: img1,
         type: "s"
     }
-]
+];
 
-export default articles;
+const card0 = document.getElementById("card--0");
+const card1 = document.getElementById("card--1");
+const card2 = document.getElementById("card--2");
+const card3 = document.getElementById("card--3");
+const card4 = document.getElementById("card--4");
+const card5 = document.getElementById("card--5");
+const card6 = document.getElementById("card--6");
+const card7 = document.getElementById("card--7");
+const card8 = document.getElementById("card--8");
+const card9 = document.getElementById("card--9");
+
+const cards = [card0, card1, card2, card3, card4, card5, card6, card7, card8, card9];
+
+function combine (cardId, id) {
+    const cardTitle = cardId.querySelector("h3").innerText = articles[id].title;
+    const cardBody = cardId.querySelector("p").innerText = articles[id].body;
+}
+
+for (let i = 0; i < articles.length; i++) {
+    combine(cards[i], i);
+}
+
