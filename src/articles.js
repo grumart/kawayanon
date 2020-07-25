02b34df7fbdd4555e02190d949d6439f4975ec90
Marta GRudzien <grudzien.mart@gmail.com>
2020-07-25 09:33:26 -0400

    cleaning

--- src/articles.js
+++ src/articles.js
@@ -45,7 +45,7 @@ const articles = [
     },
     {
         id: "7",
-        title: "what about the waist?",
+        title: "what about the waste?",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         // imgsrc: img1,
         type: "s"
@@ -66,7 +66,7 @@ const articles = [
     },
     {
         id: "10",
-        title: "salamata!",
+        title: "salamat!",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         // imgsrc: img1,
         type: "s"
