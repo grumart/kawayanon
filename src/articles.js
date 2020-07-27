c231ee010fdfbc59d6ce69b91da4e5dae5249c66
Marta GRudzien <grudzien.mart@gmail.com>
2020-07-27 14:01:12 -0400

    changing cards

--- src/articles.js
+++ src/articles.js
@@ -2,70 +2,70 @@
 
 const articles = [
     {
-        id: "1",
+        id: "0",
         title: "little about us",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         // imgsrc: img1,
         type: "xl"
     },
     {
-        id: "2",
+        id: "1",
         title: "no more plastic water",
         body: "Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-        // imgsrc: img1,
+        // imgsrc: "../img/article-2.jpg",
         type: "m"
     },
     {
-        id: "3",
+        id: "2",
         title: "cooking on the island",
         body: "Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         // imgsrc: img1,
         type: "m"
     },
     {
-        id: "4",
+        id: "3",
         title: "solar electricity",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         // imgsrc: img1,
         type: "m"
     },
     {
-        id: "5",
+        id: "4",
         title: "what is kawayanan?",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         // imgsrc: img1,
         type: "l"
     },
     {
-        id: "6",
+        id: "5",
         title: "why bamboo?",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         // imgsrc: img1,
         type: "l"
     },
     {
-        id: "7",
+        id: "6",
         title: "what about the waste?",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         // imgsrc: img1,
         type: "s"
     },
     {
-        id: "8",
+        id: "7",
         title: "our little garden",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         // imgsrc: img1,
         type: "s"
     },
     {
-        id: "9",
+        id: "8",
         title: "why this way?",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         // imgsrc: img1,
         type: "s"
     },
     {
-        id: "10",
+        id: "9",
         title: "salamat!",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         // imgsrc: img1,
@@ -73,25 +73,44 @@ const articles = [
     }
 ];
 
-const card0 = document.getElementById("card--0");
-const card1 = document.getElementById("card--1");
-const card2 = document.getElementById("card--2");
-const card3 = document.getElementById("card--3");
-const card4 = document.getElementById("card--4");
-const card5 = document.getElementById("card--5");
-const card6 = document.getElementById("card--6");
-const card7 = document.getElementById("card--7");
-const card8 = document.getElementById("card--8");
-const card9 = document.getElementById("card--9");
+const card0 = document.getElementById("card0");
+const card1 = document.getElementById("card1");
+const card2 = document.getElementById("card2");
+const card3 = document.getElementById("card3");
+const card4 = document.getElementById("card4");
+const card5 = document.getElementById("card5");
+const card6 = document.getElementById("card6");
+const card7 = document.getElementById("card7");
+const card8 = document.getElementById("card8");
+const card9 = document.getElementById("card9");
 
 const cards = [card0, card1, card2, card3, card4, card5, card6, card7, card8, card9];
 
 function combine (cardId, id) {
     const cardTitle = cardId.querySelector("h3").innerText = articles[id].title;
-    const cardBody = cardId.querySelector("p").innerText = articles[id].body;
+    const cardDescription = cardId.querySelector("p").innerText = articles[id].body;
 }
 
 for (let i = 0; i < articles.length; i++) {
     combine(cards[i], i);
 }
 
+// const test = document.getElementById("test");
+// const popup = document.getElementById("popup");
+// const testBtn = test.querySelector("button");
+
+// testBtn.addEventListener("click", () => {
+//     console.log(testBtn.id);
+//     popup.style.opacity = "1";
+//     popup.style.visibility = "initial";
+
+//     const popupTitle = popup.querySelector("h3").innerText = articles[testBtn.id].title;
+//     const popupContent = popup.querySelector("p").innerText = articles[testBtn.id].body;
+
+//     const closeBtn = popup.querySelector("button");
+
+//     closeBtn.addEventListener("click", () => {
+//         popup.style.opacity = "0";
+//         popup.style.visibility = "hidden";
+//     })
+// })
