941e087d9ba18234e0291f7af92f20b44de51597
Marta GRudzien <grudzien.mart@gmail.com>
2020-07-30 10:55:01 -0400

    articles changed

--- src/articles.js
+++ src/articles.js
@@ -1,13 +1,13 @@
 const articles = [
     {
         id: "0",
-        title: "little about us",
+        title: "payag - bamboo hut",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         description: "This will be a short description of current article"
     },
     {
         id: "1",
-        title: "no more plastic water",
+        title: "balay - off grid house",
         body: "Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         description: "This will be a short description of current article"
     },
@@ -49,7 +49,7 @@ const articles = [
     },
     {
         id: "8",
-        title: "why this way?",
+        title: "no more plastic water",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         description: "This will be a short description of current article"
     },
